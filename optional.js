0a3a15804242f1e790a58fe8a6b6fcb2ef0d1b4b
Krassi <kfotev@peerbelt.com>
2015-08-17 07:53:34 -0400

    direct parent can be specified as a n option

--- optional.js
+++ optional.js
@@ -1,6 +1,6 @@
 module.exports = function(newModule, options){
   try{
-    return module.parent.require(newModule);
+    return ( ( options && options.parent ) || module.parent).require(newModule);
   }catch(err){ 
     if (err.code !== "MODULE_NOT_FOUND" && options && options.rethrow) {
       throw err;
